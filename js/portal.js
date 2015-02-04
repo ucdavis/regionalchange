af4a2900f31b08373d298c6d450a3c87c4add159
Cal <cydoval@mac.com>
2015-02-04 13:42:47 -0800

    first push of code

---
+++ js/portal.js
@@ -0,0 +1,68 @@
+// JavaScript Document
+$(document).ready(function() {
+	
+
+
+//explore
+	$('#mm').hover(function(){
+		//change text
+		$('#map').toggle();
+	
+	});
+	
+	
+$("#mm").hover(function(){
+  $("#one").css("background-color","#993366");
+  },function(){
+  $("#one").css("background-color","none");
+}); 
+
+
+
+//interact
+$('#pp').hover(function(){
+		//change text
+		$('#pyom').toggle();
+	});
+
+$("#pp").hover(function(){
+  $("#two").css("background-color","#3366CC");
+  },function(){
+  $("#two").css("background-color","none");
+}); 
+
+$('#pyomH').hover(function(){
+		//change text
+		$('#pyomP').toggle();
+	});
+$('#roiH').hover(function(){
+		//change text
+		$('#roiP').toggle();
+	});
+$('#ccep').hover(function(){
+		//change text
+		$('#ccepP').toggle();
+	});
+$('#ej').hover(function(){
+		//change text
+		$('#ejP').toggle();
+	});
+
+
+
+//inspire
+
+$('#ii').hover(function(){
+		//change text
+		$('#interact').toggle();
+	});
+
+$("#ii").hover(function(){
+  $("#three").css("background-color","#256E25");
+  },function(){
+  $("#three").css("background-color","none");
+}); 
+
+	
+	
+	});// JavaScript Document// JavaScript Document// JavaScript Document// JavaScript Document
